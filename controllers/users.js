1b96f5cb2a9d2039153bfeb2b2846c9a43cd171f
matthew-wright9630 <matthew.wright9630@gmail.com>
2024-09-30 15:02:31 -0400

    Fix eslint problems

--- controllers/users.js
+++ controllers/users.js
@@ -39,11 +39,8 @@ module.exports.createUser = (req, res, next) => {
 module.exports.login = (req, res, next) => {
   const { email, password } = req.body;
 
-  return User.findUserByCredentials(
-    email,
-    password
-  ) /*findUserByCredentials is a function in models/user.js */
-
+  /*findUserByCredentials is a function in models/user.js */
+  return User.findUserByCredentials(email, password)
     .then((user) => {
       res.send({
         user,
